ad332e93f5718f78ad94de2c5eaf427e0c356011
MarioMinaGaballa <109560330+MarioMinaGaballa@users.noreply.github.com>
2023-07-04 17:02:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img =document.querySelector(".imge");
+let con=document.querySelector(".container")
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+  con.style.background=color;
+}
